06c7b47db9ef24a20fcf5b4f542763da18385ee0
Fausto Sacufundala <155498985+fausto18@users.noreply.github.com>
2024-01-08 12:20:15 +0100

    Update ft_strcpy.c

--- EXAM03/5-ft_strcpy/ft_strcpy.c
+++ EXAM03/5-ft_strcpy/ft_strcpy.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
+#include <string.h>
 
 char	*ft_strcpy(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
-	while (s2[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		s1[i] = s2[i];
+		s2[i] = s1[i];
 		i++;
 	}
-	s1[i] = '\0';
-	return (s1);
+	s2[i] = '\0';
+	return (s2);
 }
